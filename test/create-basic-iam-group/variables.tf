ceeeef8bc4fe6c2a05bd85618d2e139dd702801f
Marius Tolzmann <m@rius.berlin>
2020-06-23 00:45:53 +0200

    Add unit-test to deploy a single iam group
    Co-authored-by: Jan Kyri <me@jankyri.com>

---
+++ test/create-basic-iam-group/variables.tf
@@ -0,0 +1,8 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TEST MODULE THAT IS USED BY THE UNIT TESTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+variable "aws_region" {
+  description = "The AWS region to deploy the example in."
+  type        = string
+}
